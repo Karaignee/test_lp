dc5a1aa9432b72c16cf29b7909787db3abff15fc
Karen Rooney <karaignee@gmail.com>
2015-07-14 21:27:01 +0100

    index css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+print '-- continent -- '
+  Continent.delete_all
+  Continent.where(name: "Africa").first_or_create
+print '--destination details --'
+  Destination.delete_all 
+  Destination.where(title: "one", overview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat dapibus justo, nec commodo velit volutpat ut. Vestibulum ut fermentum enim. Pellentesque sit amet lacus urna. Donec pellentesque aliquet sodales. ", before_you_go: "testing", gettin_around: "testing again").first_or_create
+
